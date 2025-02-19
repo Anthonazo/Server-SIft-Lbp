80e7a10af7393ebdea29ff9b5bf66ba97a104910
Anthony Moya <anthonymoya246@gmail.com>
2025-02-19 15:27:35 -0500

    proyecto ternimado SIFT - LBP

--- lbp.py
+++ lbp.py
@@ -2,6 +2,8 @@ from flask import Flask, Response
 import cv2
 import socket
 import numpy as np
+import json
+import os
 
 app = Flask(__name__)
 
@@ -11,19 +13,61 @@ UDP_PORT = 5005
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_PORT))
 
+def get_object_cascades(filename: str) -> dict:
+    object_cascades = {}
+    with open(filename, 'r') as fs:
+        object_cascades = json.load(fs)
+    if len(object_cascades.keys()) > 0:
+        for object_cascade_name in object_cascades.keys():
+            object_cascade_path = object_cascades[object_cascade_name]
+            if os.path.exists(object_cascade_path):
+                object_cascades[object_cascade_name] = cv2.CascadeClassifier(object_cascade_path)
+                print(f"Cargado clasificador para: {object_cascade_name}")
+            else:
+                print(f"Error: No se encontró el archivo para {object_cascade_name} en {object_cascade_path}")
+    else:
+        raise ValueError('Load cascades into cascades.json.')
+    return object_cascades
+
 def frame_generator():
     frame_buffer = bytearray()
     buffer_size = 65536
 
+    object_cascades = get_object_cascades('/home/anthony/Escritorio/Virtualenvs/env-SiftProject/src/cascades.json')
+    font = cv2.FONT_HERSHEY_SIMPLEX
+
     while True:
-        packet, _ = sock.recvfrom(buffer_size)  # Recibir paquete UDP
-        frame_buffer.extend(packet)  # Agregar los datos al buffer
+        packet, _ = sock.recvfrom(buffer_size) 
+        frame_buffer.extend(packet) 
 
         try:
             frame = cv2.imdecode(np.frombuffer(frame_buffer, np.uint8), cv2.IMREAD_COLOR)
             if frame is not None:
-                frame_buffer.clear()  # Limpiar el buffer después de procesar el frame
-                cv2.imshow('Stream de Video', frame)  # Mostrar el frame en una ventana
+                frame_buffer.clear()
+
+                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+                for object_cascade_name in object_cascades.keys():
+                    object_cascade = object_cascades[object_cascade_name]
+
+                    # Parámetros específicos para cada clasificador
+                    if object_cascade_name == "no_entry":
+                        scaleFactor = 1.4  # Prueba con un valor más bajo
+                        minNeighbors = 40   # Aumenta para reducir falsas detecciones
+                    else:
+                        scaleFactor = 1.4
+                        minNeighbors = 30
+
+                    objects = object_cascade.detectMultiScale(gray, scaleFactor=scaleFactor, minNeighbors=minNeighbors, minSize=(30, 30))
+
+                    for (x, y, w, h) in objects:
+                        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+                        text = f"{object_cascade_name}"
+                        cv2.putText(frame, text, (x + 5, y - 10), font, 0.9, (255, 255, 255), 2)
+
+
+                cv2.imshow('Stream de Video', frame)
 
                 # Esperar por la tecla 'q' para salir
                 if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -34,4 +78,4 @@ def frame_generator():
     cv2.destroyAllWindows()
 
 if __name__ == '__main__':
-    frame_generator()  
+    frame_generator()
--- sift.py
+++ sift.py
@@ -2,14 +2,22 @@ import cv2
 import socket
 import numpy as np
 
-
+# Configuración de UDP
 UDP_IP = "0.0.0.0"
 UDP_PORT = 5005
+BUFFER_SIZE = 65536  
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_PORT))
 
-original_image_path = "/home/anthony/Escritorio/Virtualenvs/env-SiftProject/src/data/cat.jpg"
+original_image_paths = {
+    'cat': "/home/anthony/Escritorio/Virtualenvs/env-SiftProject/src/data/cat.jpg",
+    'person': "/home/anthony/Escritorio/Virtualenvs/env-SiftProject/src/data/person.jpg"
+}
+
+category = 'person'  # Cambiar entre 'cat' y 'me'
+
+original_image_path = original_image_paths[category]
 original_image = cv2.imread(original_image_path, cv2.IMREAD_GRAYSCALE)
 
 resize_factor = 0.6
@@ -17,62 +25,58 @@ original_image_resized = cv2.resize(original_image, None, fx=resize_factor, fy=r
 
 sift = cv2.SIFT_create()
 
-# Extraer keypoints y descriptores de la imagen original redimensionada
 keypoints_original, descriptors_original = sift.detectAndCompute(original_image_resized, None)
 
-# Redimensionar la imagen de superposición (ajusta el tamaño según sea necesario)
 overlay_height, overlay_width = 1, 1
 overlay_image = cv2.imread(original_image_path, cv2.IMREAD_GRAYSCALE)
 overlay_image = cv2.resize(overlay_image, (overlay_width, overlay_height))
 
-# Extraer keypoints y descriptores de la imagen superpuesta
 keypoints_overlay, descriptors_overlay = sift.detectAndCompute(overlay_image, None)
 
 bf = cv2.BFMatcher(cv2.NORM_L2, crossCheck=True)
 
 def frame_generator():
-    frame_buffer = bytearray()  # Buffer para almacenar los datos del frame
-    buffer_size = 65536  # Tamaño del buffer para UDP
+    frame_buffer = bytearray()
 
     while True:
-        packet, _ = sock.recvfrom(buffer_size)  # Recibir paquete UDP
-        frame_buffer.extend(packet)  # Agregar los datos al buffer
-
-        # Intentar decodificar el frame cuando el buffer tiene suficientes datos
+        packet, _ = sock.recvfrom(BUFFER_SIZE)
+        frame_buffer.extend(packet) 
         try:
             frame = cv2.imdecode(np.frombuffer(frame_buffer, np.uint8), cv2.IMREAD_COLOR)
             if frame is not None:
-                frame_buffer.clear()  # Limpiar el buffer después de procesar el frame
+                frame_buffer.clear() 
 
-                # Convertir el frame a escala de grises para el procesamiento SIFT
                 frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-                # Obtener las dimensiones del frame
                 frame_height, frame_width = frame_gray.shape
 
-                # Calcular la posición para centrar la imagen superpuesta
                 x_offset = (frame_width - overlay_width) // 2
                 y_offset = (frame_height - overlay_height) // 2
 
-                # Superponer la imagen en el frame
                 frame_gray[y_offset:y_offset + overlay_height, x_offset:x_offset + overlay_width] = overlay_image
 
-                # Extraer keypoints y descriptores del frame actual (con la imagen superpuesta)
                 keypoints_frame, descriptors_frame = sift.detectAndCompute(frame_gray, None)
 
-                # Emparejar las características entre la imagen original y el frame
                 matches = bf.match(descriptors_original, descriptors_frame)
 
-                # Ordenar los matches por distancia (mejores coincidencias primero)
                 matches = sorted(matches, key=lambda x: x.distance)
 
-                # Dibujar las coincidencias en el frame
                 matched_frame = cv2.drawMatches(
-                    original_image_resized, keypoints_original,  # Imagen original redimensionada y sus keypoints
-                    frame_gray, keypoints_frame,                # Frame actual y sus keypoints
-                    matches[:35], None,                         # Mostrar solo las 10 mejores coincidencias
+                    original_image_resized, keypoints_original,  
+                    frame_gray, keypoints_frame,              
+                    matches[:35], None,                   
                     flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS
                 )
+
+                category_matches = 0
+                for m in matches:
+                    if m.distance < 100:
+                        category_matches += 1
+
+                if category_matches > 10:
+                    cv2.putText(matched_frame, f"Categoria detectada: {category} con {category_matches} coincidencias", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2)
+                else:
+                    cv2.putText(matched_frame, "Categoria desconocida", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 2)
 
                 cv2.imshow('Video con SIFT', matched_frame)
 
@@ -84,4 +88,4 @@ def frame_generator():
     cv2.destroyAllWindows()
 
 if __name__ == '__main__':
-    frame_generator() 
+    frame_generator()
